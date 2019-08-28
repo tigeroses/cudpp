8a439fe03bfee5c780a8c4bdda626f100295f04f
fxzhao <zhaofuxiang@genomics.cn>
2019-08-28 11:28:21 +0800

    change basic hash table: change key type from unsigned int to unsigned long long ,but use the lower 48 bit change value type from unsigned int to unsigned short, it has 16 bit and encode the key and value as Entry, it's type is unsigned long long

--- src/cudpp_hash/hash_table.cpp
+++ src/cudpp_hash/hash_table.cpp
@@ -252,6 +252,110 @@ bool HashTable::Build(const unsigned  n,
 }
 
 
+bool HashTable::Build(const unsigned  n,
+	const unsigned long long *d_keys,
+	const unsigned short *d_values) {
+	unsigned max_iterations = ComputeMaxIterations(n, table_size_,
+		num_hash_functions_);
+	unsigned num_failures = 1;
+	unsigned num_attempts = 0;
+
+	// Storage for statistics collection.
+	unsigned *d_iterations_taken = NULL;
+#ifdef TRACK_ITERATIONS
+	CUDA_SAFE_CALL(cudaMalloc((void**)&d_iterations_taken, sizeof(unsigned) * n));
+#endif
+
+	// Track how many items ended up in the stash.
+	unsigned *d_stash_count = NULL;
+	CUDA_SAFE_CALL(cudaMalloc((void**)&d_stash_count, sizeof(unsigned)));
+	CUDA_CHECK_ERROR("Failed before main build loop.\n");
+
+	// Main build loop.
+	while (num_failures && ++num_attempts < kMaxRestartAttempts) {
+		CUDA_SAFE_CALL(cudaMemset(d_stash_count, 0, sizeof(unsigned)));
+
+		// Generate new hash functions.
+		if (num_hash_functions_ == 2)
+			constants_2_.Generate(n, d_keys, table_size_);
+		else if (num_hash_functions_ == 3)
+			constants_3_.Generate(n, d_keys, table_size_);
+		else if (num_hash_functions_ == 4)
+			constants_4_.Generate(n, d_keys, table_size_);
+		else
+			constants_5_.Generate(n, d_keys, table_size_);
+
+		stash_constants_.x = std::max(1lu, genrand_int32()) % kPrimeDivisor;
+		stash_constants_.y = genrand_int32() % kPrimeDivisor;
+		stash_count_ = 0;
+
+		// Initialize memory.
+		unsigned slots_in_table = table_size_ + kStashSize;
+		CUDAWrapper::ClearTable(slots_in_table,
+			kEntryEmpty,
+			d_contents_);
+
+		num_failures = 0;
+
+		CUDAWrapper::CallCuckooHash(n,
+			num_hash_functions_,
+			d_keys,
+			d_values,
+			table_size_,
+			constants_2_,
+			constants_3_,
+			constants_4_,
+			constants_5_,
+			max_iterations,
+			d_contents_,
+			stash_constants_,
+			d_stash_count,
+			d_failures_,
+			d_iterations_taken);
+
+		// Check if successful.
+		CUDA_SAFE_CALL(cudaMemcpy(&num_failures, d_failures_, sizeof(unsigned), cudaMemcpyDeviceToHost));
+
+#ifdef COUNT_UNINSERTED
+		if (num_failures) {
+			printf("Failed to insert %u items.\n", num_failures);
+		}
+#endif
+	}
+
+	// Copy out the stash size.
+	CUDA_SAFE_CALL(cudaMemcpy(&stash_count_, d_stash_count, sizeof(unsigned), cudaMemcpyDeviceToHost));
+	if (stash_count_ && num_failures == 0) {
+		sprintf(buffer, "Stash size: %u", stash_count_);
+		PrintMessage(buffer, true);
+
+#ifdef _DEBUG
+		PrintStashContents(d_contents_ + table_size_);
+#endif    
+	}
+	CUDA_SAFE_CALL(cudaFree(d_stash_count));
+
+#ifdef TRACK_ITERATIONS
+	if (num_failures == 0) {
+		OutputBuildStatistics(n, d_iterations_taken);
+	}
+	CUDA_SAFE_CALL(cudaFree(d_iterations_taken));
+#endif
+
+	// Dump some info if a restart was required.
+	if (num_attempts >= kMaxRestartAttempts) {
+		sprintf(buffer, "Completely failed to build");
+		PrintMessage(buffer, true);
+	}
+	else if (num_attempts > 1) {
+		sprintf(buffer, "Needed %u attempts to build", num_attempts);
+		PrintMessage(buffer, true);
+	}
+
+	CUDA_CHECK_ERROR("Error occurred during hash table build.\n");
+	return num_failures == 0;
+}
+
 void HashTable::Retrieve(const unsigned  n_queries,
                          const unsigned *d_keys,
                          unsigned *d_values) {
@@ -267,6 +371,23 @@ void HashTable::Retrieve(const unsigned  n_queries,
                                   stash_constants_,
                                   stash_count_,
                                   d_values);
+}
+
+void HashTable::Retrieve(const unsigned  n_queries,
+	const unsigned long long *d_keys,
+	unsigned short *d_values) {
+	CUDAWrapper::CallHashRetrieve(n_queries,
+		num_hash_functions_,
+		d_keys,
+		table_size_,
+		d_contents_,
+		constants_2_,
+		constants_3_,
+		constants_4_,
+		constants_5_,
+		stash_constants_,
+		stash_count_,
+		d_values);
 }
 
 
--- src/cudpp_hash/hash_table.cu
+++ src/cudpp_hash/hash_table.cu
@@ -107,6 +107,83 @@ namespace CUDAWrapper {
         CUDA_CHECK_ERROR("Error occurred during hash table build.\n");
     }
 
+	void CallCuckooHash(const unsigned      n,
+		const unsigned      num_hash_functions,
+		const unsigned long long     *d_keys,
+		const unsigned short    *d_values,
+		const unsigned      table_size,
+		const Functions<2>  constants_2,
+		const Functions<3>  constants_3,
+		const Functions<4>  constants_4,
+		const Functions<5>  constants_5,
+		const unsigned      max_iterations,
+		Entry        *d_contents,
+		uint2         stash_constants,
+		unsigned     *d_stash_count,
+		unsigned     *d_failures,
+		unsigned     *d_iterations_taken) {
+		// Build the table.
+		cudaMemset(d_failures, 0, sizeof(unsigned));
+		if (num_hash_functions == 2) {
+			CuckooHash << <ComputeGridDim(n), kBlockSize >> >
+				(n,
+					d_keys,
+					d_values,
+					table_size,
+					constants_2,
+					max_iterations,
+					d_contents,
+					stash_constants,
+					d_stash_count,
+					d_failures,
+					d_iterations_taken);
+		}
+		else if (num_hash_functions == 3) {
+			CuckooHash << <ComputeGridDim(n), kBlockSize >> >
+				(n,
+					d_keys,
+					d_values,
+					table_size,
+					constants_3,
+					max_iterations,
+					d_contents,
+					stash_constants,
+					d_stash_count,
+					d_failures,
+					d_iterations_taken);
+		}
+		else if (num_hash_functions == 4) {
+			CuckooHash << <ComputeGridDim(n), kBlockSize >> >
+				(n,
+					d_keys,
+					d_values,
+					table_size,
+					constants_4,
+					max_iterations,
+					d_contents,
+					stash_constants,
+					d_stash_count,
+					d_failures,
+					d_iterations_taken);
+		}
+		else {
+			CuckooHash << <ComputeGridDim(n), kBlockSize >> >
+				(n,
+					d_keys,
+					d_values,
+					table_size,
+					constants_5,
+					max_iterations,
+					d_contents,
+					stash_constants,
+					d_stash_count,
+					d_failures,
+					d_iterations_taken);
+		}
+
+		CUDA_CHECK_ERROR("Error occurred during hash table build.\n");
+	}
+
 
     void CallHashRetrieve(const unsigned      n_queries,
                           const unsigned      num_hash_functions,
@@ -180,6 +257,82 @@ namespace CUDAWrapper {
         CUDA_SAFE_CALL(cudaFree(d_retrieval_probes));
     #endif
     }
+
+	void CallHashRetrieve(const unsigned      n_queries,
+		const unsigned      num_hash_functions,
+		const unsigned long long     *d_keys,
+		const unsigned      table_size,
+		const Entry        *d_contents,
+		const Functions<2>  constants_2,
+		const Functions<3>  constants_3,
+		const Functions<4>  constants_4,
+		const Functions<5>  constants_5,
+		const uint2         stash_constants,
+		const unsigned      stash_count,
+		unsigned short     *d_values) {
+		unsigned *d_retrieval_probes = NULL;
+#ifdef TRACK_ITERATIONS
+		CUDA_SAFE_CALL(cudaMalloc((void**)&d_retrieval_probes, sizeof(unsigned) * n_queries));
+#endif
+
+		if (num_hash_functions == 2) {
+			hash_retrieve << <ComputeGridDim(n_queries), kBlockSize >> >
+				(n_queries,
+					d_keys,
+					table_size,
+					d_contents,
+					constants_2,
+					stash_constants,
+					stash_count,
+					d_values,
+					d_retrieval_probes);
+		}
+		else if (num_hash_functions == 3) {
+			hash_retrieve << <ComputeGridDim(n_queries), kBlockSize >> >
+				(n_queries,
+					d_keys,
+					table_size,
+					d_contents,
+					constants_3,
+					stash_constants,
+					stash_count,
+					d_values,
+					d_retrieval_probes);
+		}
+		else if (num_hash_functions == 4) {
+			hash_retrieve << <ComputeGridDim(n_queries), kBlockSize >> >
+				(n_queries,
+					d_keys,
+					table_size,
+					d_contents,
+					constants_4,
+					stash_constants,
+					stash_count,
+					d_values,
+					d_retrieval_probes);
+		}
+		else {
+			hash_retrieve << <ComputeGridDim(n_queries), kBlockSize >> >
+				(n_queries,
+					d_keys,
+					table_size,
+					d_contents,
+					constants_5,
+					stash_constants,
+					stash_count,
+					d_values,
+					d_retrieval_probes);
+		}
+
+		CUDA_CHECK_ERROR("Retrieval failed.\n");
+
+#ifdef TRACK_ITERATIONS
+		OutputRetrievalStatistics(n_queries,
+			d_retrieval_probes,
+			num_hash_functions);
+		CUDA_SAFE_CALL(cudaFree(d_retrieval_probes));
+#endif
+	}
 };  // namespace CUDAWrapper
 
 
